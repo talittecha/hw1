04b99277426c7c757af01f03f52c060742c18d8b
talittecha <ttechawanwekin@cca.edu>
2015-09-06 00:44:35 -0700

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,38 @@
+var a = 0;
+while (a < 8) {
+  down();
+  a = a + 1;
+}
+a = 0;
+right();
+right();
+
+while (a < 7) {
+  up();
+  a = a + 1;
+}
+a = 0;
+right();
+right();
+
+while (a < 7) {
+  down();
+  a = a + 1;
+}
+a = 0; 
+right();
+right();
+
+while (a < 7) {
+  up();
+  a = a + 1;
+}
+a = 0; 
+right();
+right();
+
+while (a < 7) {
+  down();
+  a = a + 1;
+} 
+right();

b25084a3bfdebd6b5f034477aa56894ef682acf8
talittecha <ttechawanwekin@cca.edu>
2015-09-06 00:40:51 -0700

    Create puzzle4.js

---
+++ puzzle4.js
@@ -0,0 +1,43 @@
+down();down();down();down();
+right(); 
+if (getColor() == "blue") {
+  down();
+  setColor('blue');
+} if (getColor() == "green") 
+{
+  down();
+  setColor('green');
+}
+if (getColor() == "pink") 
+{
+  down();
+  setColor('pink');
+}
+if (getColor() == "yellow") 
+{
+  down();
+  setColor('yellow');
+}
+right();
+right();
+right();
+up();
+if (getColor() == "blue") {
+  down();
+  setColor('blue');
+} if (getColor() == "green") 
+{
+  down();
+  setColor('green');
+}
+if (getColor() == "pink") 
+{
+  down();
+  setColor('pink');
+}
+if (getColor() == "yellow") 
+{
+  down();
+  setColor('yellow');
+}
+right();right();

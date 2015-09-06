c4dfa7d4ec0864361afb028f20ce0c8e6b9c098d
talittecha <ttechawanwekin@cca.edu>
2015-09-06 00:42:36 -0700

    Create puzzle7.js

---
+++ puzzle7.js
@@ -0,0 +1,39 @@
+var a = "";
+while (a != "red") {
+  down();
+  a = getColor();
+}
+a= "";
+right();
+right();
+
+while (a != "red") {
+  up();
+  a = getColor();
+}
+a= "";
+right();
+right();
+
+while (a != "red") {
+  down();
+  a = getColor();
+}
+a= ""; 
+right();
+right();
+
+while (a != "red") {
+  up();
+  a = getColor();
+}
+a= ""; 
+right();
+right();
+
+while (a != "red") {
+  down();
+  a = getColor();
+}
+a= ""; 
+right();

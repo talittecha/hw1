0522ff7fcc5c2406d92460349a33d55baede4d41
talittecha <ttechawanwekin@cca.edu>
2015-09-06 00:42:03 -0700

    Create puzzle6.js

---
+++ puzzle6.js
@@ -0,0 +1,20 @@
+right();
+if (getColor() == "blue") {
+  down();
+  down();
+  down();
+} else {
+  if (getColor() == "pink") {
+  up();
+} else {
+  up();
+  up();
+  up();
+}
+}
+if (getColor() == "blue") {
+  right();
+  } else {
+  left();
+  down();
+}
